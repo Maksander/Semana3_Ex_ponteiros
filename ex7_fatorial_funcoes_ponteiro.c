ee68f7af14c499f057d6b24209b3e007d14dc946
marksander <marktrambaiolli@hotmail.com>
2020-03-08 15:54:25 -0300

    ex7 alterado para trabalhar com ponteiros

--- ex7_fatorial_funcoes_ponteiro.c
+++ ex7_fatorial_funcoes_ponteiro.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
-double fatorial(int ent){
+double fatorial(int *ent){
 	double fatorial;
-	fatorial=ent;
-	ent--;
-	while(ent>1){
-		fatorial = fatorial * ent;
-		ent--;
+	
+	double *p_fatorial=&fatorial;
+	
+	*p_fatorial = *ent;
+	(*ent)--;
+	while((*ent)>1){
+		*p_fatorial = *p_fatorial * *ent;
+		(*ent)--;
 	}
-	return fatorial;
+	return *p_fatorial;	//retornar o endereço, pq a função retornar 
 };
 
 void main(){
     int x;
-    scanf("%d",&x);
-	printf("%.lf\n",fatorial(x));
+    
+    int *p_x = &x;
+    
+    scanf("%d",p_x);
+	printf("%.lf\n",fatorial(p_x));
 }
 
 
