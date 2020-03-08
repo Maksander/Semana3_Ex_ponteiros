9c4bf08e55c7a4356e623d5d300f8e1dae32a01a
marksander <marktrambaiolli@hotmail.com>
2020-03-08 16:06:28 -0300

    Terminado Ex8, para uso com ponteiro

--- ex8_Primo_funcoes_ponteiro.c
+++ ex8_Primo_funcoes_ponteiro.c
@@ -1,23 +1,31 @@
 #include<stdio.h>
 #include<math.h>
 
-char* Primo(int n){
+char* Primo(int* n){
 	int i,maxI, resultado=1;
-	if(n%2==0||n%3==0)resultado=0;
-	if(n==2||n==3)resultado=1;
-	maxI = sqrt(n)+2;
-	//printf("%d",maxI);
-	for(i=5; i<maxI; i+=6){
-		if(n%i==0||n%(i+2)==0) resultado=0;
+	
+	int *p_i = &i;
+	int *p_maxI = &maxI;
+	int *p_resultado = &resultado;
+	
+	if(*n%2==0||*n%3==0)*p_resultado=0;
+	if(*n==2||*n==3)*p_resultado=1;
+	*p_maxI = sqrt(*n)+2;
+	//printf("%d",*p_maxI);
+	for(*p_i=5; *p_i < *p_maxI; (*p_i)+=6){
+		if(*n%i==0||*n%(i+2)==0) *p_resultado=0;
 	}
-	if(resultado==1)return "Primo";
+	if(*p_resultado==1)return "Primo";
 	else return "Nao Primo";
 };
 
 void main(){
     int x;
-    scanf("%d",&x);
-	printf("%s\n",Primo(x));
+    
+    int *p_x=&x;
+    
+    scanf("%d",p_x);
+	printf("%s\n",Primo(p_x));
 }
 
 
