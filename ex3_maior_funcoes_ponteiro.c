1365b2d72551f78f3c813cac356d00813769052a
marksander <marktrambaiolli@hotmail.com>
2020-03-08 14:49:27 -0300

    adicionando o ex3 de funcoes para trabalhar com ponteiros

---
+++ ex3_maior_funcoes_ponteiro.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void maior_numero(int a, int b){
+    if (a > b) printf("%d\n",a);
+    else printf("%d\n",a);
+};
+
+void maior_numero_v2(int a, int b){
+    printf("%d\n",a > b ? a : b);
+};
+
+void maior_numero_v3(int a, int b){
+    switch (a > b){
+        case 0:
+            printf("%d\n",b);
+            break;
+        default:
+            printf("%d\n",a);
+            break;
+    }
+};
